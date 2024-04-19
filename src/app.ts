a225b01e453ecaa50c01b2069c085f701501aa59
Eugênio Moreira <me@eugeniomoreira.com>
2024-04-19 17:45:04 -0300

    revert: uncomment cors & helmet libs

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,7 @@
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
+import helmet from "helmet";
 import { userRouter } from "./router/user.router";
 
 dotenv.config();
@@ -7,8 +9,8 @@ const PORT = parseInt((process.env.SERVER_PORT as string) || "8080", 10);
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use(cors());
-// app.use(helmet());
+app.use(cors());
+app.use(helmet());
 app.use("/api/v1", userRouter);
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
